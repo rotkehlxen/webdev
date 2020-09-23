555fc5cb280e3429bd3479f20d65e94c87a77f80
Franziska Witzel <franziska.witzel@gmail.com>
2020-09-23 15:47:44 +0200

    Initial state

--- javaS/script.js
+++ javaS/script.js
@@ -33,9 +33,8 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-
+console.log("start")
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
