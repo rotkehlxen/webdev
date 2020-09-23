c5d9027a4c95e20f3ebfdcc2e821d7c77ba3017d
Franziska Witzel <franziska.witzel@gmail.com>
2020-09-23 15:50:49 +0200

    Step 1: IIFE around entire script.js code

--- javaS/script.js
+++ javaS/script.js
@@ -33,8 +33,10 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-console.log("start")
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+(function () { 
+
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
@@ -56,9 +58,10 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  } else {
-    // helloSpeaker.xxxx
-  }
-}
+    if (/* fill in condition here */) {
+      // byeSpeaker.xxxx
+    } else {
+      // helloSpeaker.xxxx
+    }
+  } 
+} )();
